73412ea58f19303f9f904c2cb40dc94238fd8361
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2023-11-12 17:06:51 +0900

    Update setup_kubeflow.sh

--- setup_kubeflow.sh
+++ setup_kubeflow.sh
@@ -30,6 +30,22 @@ cat << EOF >> ~/manifests/common/istio-1-16/kubeflow-istio-resources/base/kf-ist
 EOF
 
 # enable kubeflow to be accessed through https (2)
+cat << EOF >> ~/manifests/common/istio-1-17/kubeflow-istio-resources/base/kf-istio-resources.yaml
+    tls:
+      httpsRedirect: true
+  - hosts:
+    - '*'
+    port:
+      name: https
+      number: 443
+      protocol: HTTPS
+    tls:
+      mode: SIMPLE
+      privateKey: /etc/istio/ingressgateway-certs/tls.key
+      serverCertificate: /etc/istio/ingressgateway-certs/tls.crt
+EOF
+
+# enable kubeflow to be accessed through http
 #sed -i "s/true/false/g" ~/manifests/apps/jupyter/jupyter-web-app/upstream/base/params.env
 #sed -i "s/true/false/g" ~/manifests/apps/volumes-web-app/upstream/base/params.env
 #sed -i "s/true/false/g" ~/manifests/apps/tensorboard/tensorboards-web-app/upstream/base/params.env
@@ -42,16 +58,16 @@ sed -i "1786s/: false/: true/g" ~/manifests/common/istio-cni-1-17/istio-install/
 # change service as nodeport
 sed -i "s/ClusterIP/NodePort/g" ~/manifests/common/dex/base/service.yaml
 sed -i "s/ClusterIP/NodePort/g" ~/manifests/common/istio-1-16/istio-install/base/patches/service.yaml
+sed -i "s/ClusterIP/NodePort/g" ~/manifests/common/istio-1-17/istio-install/base/patches/service.yaml
 
-# download kustomize 5.0.0 which is stable with kubeflow 1.7.0 then copy it into /bin/bash
-wget https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.0.0/kustomize_v5.0.0_linux_amd64.tar.gz
-tar -xvf kustomize_v5.0.0_linux_amd64.tar.gz
+# download kustomize 5.0.3 which is stable with kubeflow 1.8.0 then copy it into /bin/bash
+wget https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.0.3/kustomize_v5.0.3_linux_amd64.tar.gz
+tar -xvf kustomize_v5.0.3_linux_amd64.tar.gz
 sudo mv ~/kustomize /usr/bin/
 
 # install kubeflow as a single command
 cd ~/manifests
-while ! kustomize build example | awk '!/well-defined/' | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
+while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
 
 # create certification for https connection
 sed -i 's/MASTER_IP/'"${MASTER_IP}"'/g' ${CURRENT_DIR}/certificate.yaml
-kubectl apply -f ${CURRENT_DIR}/certificate.yaml
